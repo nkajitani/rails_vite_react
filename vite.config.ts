323ad1308fedf145f6273eca3ab67d1c1098e50a
nkajitani <kajitani719@gmail.com>
2025-11-10 11:57:21 +0900

    install react, and setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    react()
   ],
   server: {
     host: '0.0.0.0',
@@ -11,5 +13,5 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-  },
+  }
 })
