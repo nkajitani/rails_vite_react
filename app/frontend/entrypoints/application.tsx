9c37d8ccba1d93474d7f3974f8dbf3ab1e71d972
nkajitani <kajitani719@gmail.com>
2025-11-11 06:21:13 +0900

    replace base HTML to suite react

---
+++ app/frontend/entrypoints/application.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import App from '../router'
+
+import '../styles/login.css'
+
+
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container)
+  root.render(<App />)
+}
