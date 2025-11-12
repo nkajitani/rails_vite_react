00a821f65102a468296804ef619b3528fab2d90b
nkajitani <kajitani719@gmail.com>
2025-11-13 06:10:38 +0900

    create auth in routes

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -2,8 +2,3 @@ namespace :admin do
   resources :posts
   resources :profile, only: [ :show, :edit, :update ]
 end
-devise_for :users, path: "admin", skip: [ :registrations ], module: "api/v1/users"
-devise_scope :user do
-  post "admin/sign_up", to: "users/registrations#create", as: :api_v1_user_registration
-  post "admin/sign_in", to: "users/sessions#create",      as: :api_v1_user_session
-end
