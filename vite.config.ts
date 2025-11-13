c14d7f088ffff02a69765a14d8ec1f10fd05cb7c
nkajitani <kajitani719@gmail.com>
2025-11-13 19:47:07 +0900

    setup plugin of vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   root: 'app/frontend',
   appType: 'spa',
   plugins: [
-    react()
+    react(), tsconfigPaths(),
   ],
   server: {
     host: '0.0.0.0',
