f6b745a9403fa78bdd12fa61068cf6600d3031f7
nkajitani <kajitani719@gmail.com>
2025-11-13 09:27:00 +0900

    replace devise controller

---
+++ app/controllers/api/v1/auth/sessions_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class Api::V1::Auth::SessionsController < Devise::SessionsController
+  # protect_from_forgery with: :null_session, if: -> { request.format.json? }
+
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # for vanilla Rails app
+  # def after_sign_in_path_for(resource)
+  #   admin_root_path  # 管理画面TOPへ
+  # end
+
+  # for vanilla Rails app
+  # def after_sign_out_path_for(resource_or_scope)
+  #   new_user_session_path
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
