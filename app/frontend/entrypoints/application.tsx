6074bc0f4c1e93116cac8ac55595447495cbcd76
nkajitani <kajitani719@gmail.com>
2025-11-23 11:01:54 +0900

    setup tailwindcss

--- app/frontend/entrypoints/application.tsx
+++ app/frontend/entrypoints/application.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from '@/router'
 
 import '@/styles/auth.css'
-
+import '@/styles/tailwind.css'
 
 const container = document.getElementById('root')
 if (container) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   root: 'app/frontend',
   appType: 'spa',
   plugins: [
-    react(), tsconfigPaths(),
+    react(), tsconfigPaths(), tailwindcss(),
   ],
   server: {
     host: '0.0.0.0',
